d85257ee8335c5552adcf1dd57f3e0fe364fde0f
chandra <chandrasekhar@bijib.in>
2026-07-10 14:50:05 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -9,3 +9,35 @@ export default defineConfig({
   outDir: 'www'
  }
 })
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import dts from "vite-plugin-dts";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     dts({
+//       insertTypesEntry: true,
+//     }),
+//   ],
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, "src/index.ts"),
+//       name: "ChandraUI",
+//       fileName: "index",
+//       formats: ["es", "cjs"],
+//     },
+//     rollupOptions: {
+//       external: ["react", "react-dom"],
+//       output: {
+//         globals: {
+//           react: "React",
+//           "react-dom": "ReactDOM",
+//         },
+//       },
+//     },
+//   },
+// });
